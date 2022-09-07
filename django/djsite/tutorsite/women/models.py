526d6af5014b1fe95fc865fe6cf1f0a1022bc741
RainerDeWitt <Moonlight.bessarabov@yandex.ru>
2022-09-07 21:24:32 +0300

    weekends learning

--- django/djsite/tutorsite/women/models.py
+++ django/djsite/tutorsite/women/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
@@ -7,5 +7,16 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id':self.pk})
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
--- django/djsite/tutorsite/women/views.py
+++ django/djsite/tutorsite/women/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+from .models import *
+menu = ["О сайте", "Добавить статью", "Обратная связь","Войти"]
 
 def index(request):
-    return HttpResponse("Страница приложения women.")
+    posts = Women.objects.all()
+    return render(request,"women/index.html", {"posts":posts, "menu":menu, "title": "Главная страница"}) #путь к шаблону
+
+def about(request):
+    return render(request,"women/about.html",{"menu":menu, "title": "О сайте"})
+
 
 def categories(request,catid):
     if(request.POST):
