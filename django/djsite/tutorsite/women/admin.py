39b78d104f12ee9cbe7fc0b76143cc8533deef46
loknarash96 <loknarash@yandex.ru>
2022-09-10 11:35:19 +0300

    10 lesson

--- django/djsite/tutorsite/women/admin.py
+++ django/djsite/tutorsite/women/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id','title','time_create','photo','is_published')
+    list_display_links = ('id','title')
+    search_fields = ('title','content')
+    list_editable = ('is_published',) #кортеж
+    list_filter = ('is_published','time_create')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+    list_display_links = ('id','name')
+    search_fields = ('name',) #Кортеж, поэтому и запятая
+
+
+admin.site.register(Women, WomenAdmin,) #появилось на админ панели, вторым параметром второстепенные классы
+admin.site.register(Category,CategoryAdmin)
--- django/djsite/tutorsite/women/views.py
+++ django/djsite/tutorsite/women/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
+import women.models
 from .models import *
 menu = [
     {'title' : "О сайте", "url_name":"about"},
@@ -11,10 +12,13 @@ menu = [
 
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
     context = {
         "posts": posts,
+        "cats": cats,
         "menu": menu,
-        "title": "Главная страница"
+        "title": "Главная страница",
+        "cat_selected" : 0,
     }
     return render(request,"women/index.html", context=context) #путь к шаблону
 
@@ -35,3 +39,17 @@ def show_post(request, post_id):
 
 def pageNotFound(request,exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
+def show_category(request,cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) ==0:
+        raise Http404()
+    context = {
+        "posts": posts,
+        "cats": cats,
+        "menu": menu,
+        "title": "Отображение по рубрикам",
+        "cat_selected": cat_id,
+    }
+    return render(request,'women/index.html',context=context)
