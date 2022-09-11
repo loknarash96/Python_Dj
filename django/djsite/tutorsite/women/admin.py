49dcd179df99a93ab459c9b0cdd81275d6ab7cb2
loknarash96 <loknarash@yandex.ru>
2022-09-11 15:22:27 +0300

    отвлились файлы при пуше

--- django/djsite/tutorsite/women/admin.py
+++ django/djsite/tutorsite/women/admin.py
@@ -1,19 +1,21 @@
 from django.contrib import admin
+
 from .models import *
 
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ('id','title','time_create','photo','is_published')
-    list_display_links = ('id','title')
-    search_fields = ('title','content')
-    list_editable = ('is_published',) #кортеж
-    list_filter = ('is_published','time_create')
-    prepopulated_fields = {'slug': ("title",)}
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
+    prepopulated_fields = {"slug": ("title",)}
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id','name')
-    list_display_links = ('id','name')
-    search_fields = ('name',) #Кортеж, поэтому и запятая
-    prepopulated_fields = {'slug':("name",)} #Автозаполнение
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
 
-admin.site.register(Women, WomenAdmin,) #появилось на админ панели, вторым параметром второстепенные классы
-admin.site.register(Category,CategoryAdmin)
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
