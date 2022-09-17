afaed76646b6beb31ca41d29ded3598421ae6e5e
loknarash96 <loknarash@yandex.ru>
2022-09-17 21:16:54 +0300

    залил в git прогресс #2

--- django/djsite/tutorsite/tutorsite/urls.py
+++ django/djsite/tutorsite/tutorsite/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('', include('women.urls')),
 ]
 if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [path('__debug__',include(debug_toolbar.urls))] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = pageNotFound #исключение 404
